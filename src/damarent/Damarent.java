af16f76052436c4ec07306e1c0f971a2802b87a0
saputtraa <sauki.10124284@mahasiswa.unikom.ac.id>
2025-07-05 16:27:06 +0700

    Deskripsi singkat tentang perubahan di Damarent.java

--- src/damarent/Damarent.java
+++ src/damarent/Damarent.java
@@ -17,7 +17,8 @@ public class Damarent {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        menu_utama utama = new menu_utama();
+        utama.setVisible(true);
     }
     
 }
